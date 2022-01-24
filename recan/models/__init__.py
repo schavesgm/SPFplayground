83fc72fab4613d4e6495abdb3db1840be0386400
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-01-24 14:18:04 +0000

    Added resnet architecture. Currently not in use

--- recan/models/__init__.py
+++ recan/models/__init__.py
@@ -1,2 +1,5 @@
 # -- Import the base class for all models
 from .base import BaseModel
+
+# -- Import the residual network model
+from .resnet import ResidualNet
---
+++ recan/models/resnet.py
@@ -0,0 +1,111 @@
+import torch.nn as nn
+import torch
+
+# Import the base network module. Allow executing this file.
+try:
+    from .base import BaseModel
+except ImportError:
+    from base import BaseModel
+
+class ResidualBlock(nn.Module):
+    """ Basic residual block composed by two convolutional layers. """
+
+    def __init__(self, in_channels: int, out_channels: int, stride: int = 1, downsample = None):
+
+        # Initialise the parents module
+        super().__init__()
+
+        # Generate the architecture of the block -- The block does not change L
+        self.layers = nn.Sequential(
+            nn.Conv1d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm1d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.1),
+            nn.Conv1d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm1d(out_channels),
+        )
+
+        # Save the downsample method and the stride
+        self.downsample, self.stride = downsample, stride
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """ Residual forward pass. The last part uses the residual addition. """
+
+        # Now, produce the forward pass of the block
+        out = self.layers(x)
+
+        # If the downsample is available, then update it
+        if self.downsample is not None:
+            x = self.downsample(x)
+
+        # Apply the ReLU and return
+        return torch.relu(out + x)
+
+def get_reslayer(num_layers: int, in_channels: int, out_channels: int, stride: int = 1) -> nn.Sequential:
+    """ Generate a sequence of residual blocks. The starting block can contain a downsample
+    layer to match the dimension of the Identity operation. The downsample layer is defined
+    to be a convolution of one parameter per channel."""
+
+    # In the first layer, it may be needed to downsample
+    downsample = None
+
+    # Try downsampling the image to match the dimensions with a conv1D
+    if stride != 1 or in_channels != out_channels:
+        downsample = nn.Sequential(
+            nn.Conv1d(in_channels, out_channels, kernel_size=1, stride=stride),
+            nn.BatchNorm1d(out_channels)
+        )
+
+    # List that will contain all the layers
+    layers = [ResidualBlock(in_channels, out_channels, stride, downsample)]
+
+    # Now, concatenate more layers with input_C=output_C and stride=1
+    layers += [ResidualBlock(out_channels, out_channels, 1) for _ in range(1, num_layers)]
+
+    return nn.Sequential(*layers)
+
+class ResidualNet(BaseModel):
+    """ Basic implementation of a 18 layer Residual Network. """
+
+    def __init__(self, input_size: int, output_size: int, name: str = ''):
+
+        # Initialise the parent module
+        super().__init__(name)
+
+        # First, change the input size to 224 pixels -- Original implementation
+        self.augment = nn.Sequential(
+            nn.Linear(input_size, 224), 
+            nn.BatchNorm1d(224),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.1),
+            nn.Unflatten(1, (1, 224)),
+        )
+
+        # Now, do the first convolution
+        self.first_block = nn.Sequential(
+            nn.Conv1d(1, 64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm1d(64),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.1),
+            nn.AvgPool1d(kernel_size=3, stride=2, padding=1)
+        )
+
+        # Generate the residual network architecture
+        self.resnet_layers = nn.Sequential(
+            get_reslayer(2, 64, 64, 1),
+            get_reslayer(2, 64, 128, 2),
+            get_reslayer(6, 128, 256, 2),
+            get_reslayer(3, 256, 512, 2),
+        )
+
+        # Output of the network using adaptative pool 1d
+        self.output = nn.Sequential(
+            nn.AdaptiveAvgPool1d((1,)),
+            nn.Flatten(1, -1),
+            nn.Linear(512, 1000),
+            nn.ReLU(),
+            nn.Linear(1000, output_size),
+        )
+
+if __name__ == '__main__':
+    pass
