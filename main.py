b88da53fb6e8d01256b31d91f12a7d1d6819bf8f
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-02-02 16:32:14 +0000

    Using log as input to limit exponential decay

--- main.py
+++ main.py
@@ -23,41 +23,6 @@ plt.style.use(['science', 'ieee', 'monospace'])
 
 # TODO: Add several examples to plot instead of just one.
 
-class Model(BaseModel):
-
-    # CHANGES:
-    # -- Added Dropout with p=0.1 after all ReLU -> Check performance?
-
-    def __init__(self, input_size: int, output_size: int, name: str = 'Model'):
-
-        super().__init__(name)
-
-        # Generate the architecture: This one works at least for 2peaks only
-        # self.arch = nn.Sequential(
-        #     nn.Linear(input_size, 512), nn.ReLU(),
-        #     nn.Linear(512, 512), nn.ReLU(),
-        #     nn.Linear(512, 512), nn.ReLU(),
-        #     nn.Linear(512, 512), nn.ReLU(), # -- Added
-        #     nn.Linear(512, 512), nn.ReLU(), # -- Added
-        #     nn.Linear(512, 512), nn.ReLU(),
-        #     nn.Linear(512, output_size)
-        # )
-
-        self.arch = nn.Sequential(
-            self.get_block(input_size, 512),
-            self.get_block(512, 1024),
-            self.get_block(1024, 2056),
-            self.get_block(2056, 2056),
-            self.get_block(2056, 1024),
-            self.get_block(1024, 512),
-            nn.Linear(512, output_size),
-        )
-
-    def get_block(self, Li: int, Lo: int, p: float = 0.1) -> nn.Sequential:
-        return nn.Sequential(
-            nn.Linear(Li, Lo), nn.ReLU(), nn.BatchNorm1d(Lo), nn.Dropout(p=p),
-        )
-
 class StandardScaler:
     """ StandardScaler class that scales and descales tensors using standard normal. """
 
@@ -90,23 +55,22 @@ if __name__ == '__main__':
     gauss = GaussianAnsatz(param_A, param_M, param_W)
 
     # Generate the kernel
-    kernel = NRQCDKernel(64, 1000, [0.0, 5.0])
+    kernel = NRQCDKernel(64, 1000, [0.0, 8.0])
 
     # Generate the factory
     dataset = SPFactory([gauss for _ in range(num_peaks)], kernel)
 
     # Generate the data: Increased from 64 to 128, check performance
-    data = dataset.generate_data(100000, 128)
+    data = dataset.generate_data(50000, 64)
 
     # Generate two scalers for the data
-    C_scaler, L_scaler = StandardScaler(data.C), StandardScaler(data.L)
+    C_scaler, L_scaler = StandardScaler(data.C.log()), StandardScaler(data.L)
 
     # Wrap the dataset around a loader function
     loader = torch.utils.data.DataLoader(dataset, batch_size=256, shuffle=True)
 
     # Generate a model to train
-    # model = Model(dataset.Nt, dataset.Ns).cuda()
-    model = ResidualNet(dataset.Nt, dataset.Ns, name='ResNet').cuda()
+    model = ResidualNet(dataset.Nt, dataset.Ns, 'ResNet').cuda()
 
     # Optimiser and learning rate scheduler
     optim = torch.optim.Adam(model.parameters(), lr=0.01)
@@ -115,7 +79,7 @@ if __name__ == '__main__':
     )
 
     # Get some random examples
-    ex = torch.randint(0, dataset.Nb, (4, ))
+    ex = torch.randint(0, dataset.Nb, (4,))
 
     # Scalers for each loss function
     scale_L, scale_C, scale_R = 1.0, 100, 1.0
@@ -136,7 +100,7 @@ if __name__ == '__main__':
             optim.zero_grad()
 
             # Normalise the input and label data and move to the GPU
-            C_data, L_data = C_scaler.scale(C_data).cuda(), L_scaler.scale(L_data).cuda()
+            C_data, L_data = C_scaler.scale(C_data.log()).cuda(), L_scaler.scale(L_data).cuda()
 
             # Compute the prediction of the network -> The output will be normalised
             L_pred = model(C_data)
@@ -151,12 +115,12 @@ if __name__ == '__main__':
             loss.backward(), optim.step()
 
         epoch_loss = torch.tensor(epoch_loss)
-        sched.step(epoch_loss.median())
+        sched.step(epoch_loss.mean())
 
         # Get the learning rate
         lr = optim.param_groups[0]['lr']
 
-        print(f'Epoch {epoch + 1}: loss_med={epoch_loss.median():.6f}, lr={lr:.6f}, eta={time.time() - start}')
+        print(f'Epoch {epoch + 1}: loss={epoch_loss.mean():.6f}, lr={lr:.6f}, eta={time.time() - start}')
 
         if (epoch + 1) % 10 == 0:
 
@@ -170,13 +134,13 @@ if __name__ == '__main__':
                 fig = plt.figure(figsize=(10, 8))
 
                 # Add several axis to the figure
-                axis = [fig.add_subplot(ex.shape[0], 3, i) for i in range(1, 3 * ex.shape[0] + 1)]
+                axis = [fig.add_subplot(ex.shape[0], 2, i) for i in range(1, 2 * ex.shape[0] + 1)]
 
                 # Get the label coefficients and the correlation functions
                 L_data, C_data = dataset.L[ex, :], dataset.C[ex, :]
 
                 # Compute the predicted coefficients from the scaled correlation functions
-                L_pred = L_scaler.descale(model(C_scaler.scale(C_data.cuda()))).cpu()
+                L_pred = L_scaler.descale(model(C_scaler.scale(C_data.cuda().log()))).cpu()
 
                 # Generate the label and the predicted objects
                 data, pred = dataset.reconstruct(L_data), dataset.reconstruct(L_pred)
@@ -185,12 +149,11 @@ if __name__ == '__main__':
                 for e in range(ex.shape[0]):
 
                     # Select the corresponding axis
-                    aL, aR, aC = axis[3 * e: 3 * (e + 1)]
+                    aL, aR, = axis[2 * e: 2 * (e + 1)]
 
                     # Set some parameters in each of the axis
                     aL.set(xlabel=r'$n_s$', ylabel=r'$L(n_s)$')
                     aR.set(xlabel=r'$\omega$', ylabel=r'$\rho(\omega)$')
-                    aC.set(xlabel=r'$\tau$', ylabel=r'$C(\tau)$', yscale='log')
 
                     # Plot the coefficients
                     aL.plot(L_data[e, :], color='blue')
@@ -199,10 +162,6 @@ if __name__ == '__main__':
                     # Plot the spectral functions
                     aR.plot(kernel.omega, data.R[e, :], color='blue')
                     aR.plot(kernel.omega, pred.R[e, :], color='red')
-
-                    # Plot the correlation functions
-                    aC.plot(kernel.tau, C_data[e, :], color='blue')
-                    aC.plot(kernel.tau, pred.C[e, :], color='red')
 
                 # Make the plot nicer
                 fig.tight_layout()
